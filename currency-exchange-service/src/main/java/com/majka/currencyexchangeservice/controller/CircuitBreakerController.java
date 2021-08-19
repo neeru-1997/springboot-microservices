ccd0aff443676f63f847863324308872b02ec3d8
Neeraj <Neeraj@github.com>
2021-08-19 16:17:36 +0530

    Resilience4j-Circuit Breaker

--- currency-exchange-service/src/main/java/com/majka/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/majka/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,6 +1,6 @@
 package com.majka.currencyexchangeservice.controller;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +14,8 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "default", fallbackMethod = "hardcodedResponse")
+//    @Retry(name = "default", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
     public String sampleApi(){
         logger.info("Sample Api call received");
         ResponseEntity<String> forEntity = new RestTemplate()
